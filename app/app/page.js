9b128c482492947010f86cc3f5ba3c3df571c570
FarukBirma <farukumarbirma@gmail.com>
2025-06-19 10:47:11 +0100

    Add homepage

---
+++ app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to QuickCart 🛒</h1>
+      <p>Your next-gen e-commerce experience is coming soon.</p>
+    </main>
+  );
+}
